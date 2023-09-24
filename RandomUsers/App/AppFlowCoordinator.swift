35fd43313c649378aa6933d30925ccc4beac68e8
Insu Park <facessu@gmail.com>
2023-09-24 17:15:22 +0900

    Move resources files, Remove ViewController and Configure required files in App Folder

---
+++ RandomUsers/App/AppFlowCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppFlowCoordinator.swift
+//  RandomUsers
+//
+//  Created by Insu Park on 2023/09/24.
+//
+
+import Foundation
---
+++ RandomUsers/App/Dependencies/AppDIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  AppDIContainer.swift
+//  RandomUsers
+//
+//  Created by Insu Park on 2023/09/24.
+//
+
+import Foundation
